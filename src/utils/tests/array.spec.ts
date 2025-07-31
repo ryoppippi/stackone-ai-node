e5cd373348926853bf7c92f703d3d061cdd8312c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-07-31 15:12:12 +0100

    test: simplify toArray test (#78)

--- src/utils/tests/array.spec.ts
+++ src/utils/tests/array.spec.ts
@@ -2,39 +2,16 @@ import { describe, expect, it } from 'bun:test';
 import { toArray } from '../array';
 
 describe('toArray', () => {
-  it('should return empty array for null or undefined', () => {
-    expect(toArray(null)).toEqual([]);
-    expect(toArray(undefined)).toEqual([]);
-    expect(toArray()).toEqual([]);
-  });
-
-  it('should return the same array if input is already an array', () => {
-    const arr = [1, 2, 3];
-    const result = toArray(arr);
-    expect(result).toBe(arr); // Should be the same reference
-    expect(result).toEqual([1, 2, 3]);
-  });
-
-  it('should wrap non-array values in an array', () => {
-    expect(toArray(1)).toEqual([1]);
-    expect(toArray('hello')).toEqual(['hello']);
-    expect(toArray({ foo: 'bar' })).toEqual([{ foo: 'bar' }]);
-    expect(toArray(true)).toEqual([true]);
-  });
-
-  it('should handle empty arrays', () => {
-    const emptyArray: any[] = [];
-    expect(toArray(emptyArray)).toBe(emptyArray);
-    expect(toArray(emptyArray)).toEqual([]);
-  });
-
-  it('should preserve types', () => {
-    const stringResult = toArray('test');
-    // TypeScript should infer this as string[]
-    const _: string[] = stringResult;
-
-    const numberArrayResult = toArray([1, 2, 3]);
-    // TypeScript should infer this as number[]
-    const __: number[] = numberArrayResult;
+  it.each([
+    [undefined, []],
+    [null, []],
+    [false, [false]],
+    [0, [0]],
+    ['', ['']],
+    [[], []],
+    ['foo', ['foo']],
+    [['foo'], ['foo']],
+  ])('%s => %s', (input, expected) => {
+    expect(toArray(input)).toEqual(expected);
   });
 });
